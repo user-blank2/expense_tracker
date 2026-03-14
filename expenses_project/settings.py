5e0565d3d41964c3f65e3808aa34159df6ea93f2
user-deepak-09 <deepak2007cbe@gmail.com>
2026-03-14 23:42:33 +0530

    Fix Vercel config and add debug logging

--- expenses_project/settings.py
+++ expenses_project/settings.py
@@ -136,3 +136,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
