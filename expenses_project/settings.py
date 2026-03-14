10ef764adde480dce2f13e96e8d376700d242316
user-deepak-09 <deepak2007cbe@gmail.com>
2026-03-14 23:22:42 +0530

    Remove secret key from settings, use environment variable only

--- expenses_project/settings.py
+++ expenses_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY','local-dev-key-change-in-production')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG','FALSE') == 'TRUE'
 
